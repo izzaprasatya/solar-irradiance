69212ff63a835cb5df63393a3eae02f243806ae2
prasatyaizza <prasatyaizza@gmail.com>
2025-03-23 21:58:56 +0300

    Add files via upload

---
+++ average-radiation.py
@@ -0,0 +1,13 @@
+import grass.script as gscript
+
+# Define the output raster name for the monthly average
+output_raster = "rad_insol_2_avg"
+
+# Generate the list of input rasters (rad_glob1 to rad_glob31)
+input_rasters = [f"rad_insol{i}" for i in range(32, 60)]
+
+# Run r.series to compute the average of the 31 rasters
+gscript.run_command("r.series",
+                     input=",".join(input_rasters),
+                     output=output_raster,
+                     method="average")
